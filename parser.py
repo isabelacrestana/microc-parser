6dc073479f30a547cea5d6f8589bc8a34f5156c7
isabelacrestana <crestanaisabela@gmail.com>
2026-09-14 19:00:55 -0300

    feat: implement parse_parameter_list() and parse_parameter()

--- parser.py
+++ parser.py
@@ -171,11 +171,28 @@ class Parser:
         token = self.expect(TYPE_START)
         return TYPE_BY_TOKEN[token.kind]
 
+    # parameter_list ::= parameter (COMMA parameter)*
     def parse_parameter_list(self) -> list[Parameter]:
-        raise NotImplementedError("implemente parameter_list")
+        parameters: list[Parameter] = []
+        parameters.append(self.parse_parameter())
 
+        while self.match(TokenKind.COMMA):
+            parameters.append(self.parse_parameter())
+            
+        return parameters
+
+    # parameter ::= type IDENTIFIER
     def parse_parameter(self) -> Parameter:
-        raise NotImplementedError("implemente parameter")
+        start = self.peek()
+
+        param_type = self.parse_type()
+        name = self.expect(TokenKind.IDENTIFIER)
+
+        return Parameter(
+            param_type,
+            name.lexeme,
+            span=self._span(start, name),
+        )
 
     def parse_block(self) -> Block:
         raise NotImplementedError("implemente block")
